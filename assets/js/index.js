22fccca8dbc0c6f0a334b22fc0e63b3b0315e5c7
isidora franchini <n.franchinic@gmail.com>
2024-09-07 14:48:17 -0400

    Desafío IntroJS

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+var precio = 599990
+var cantidad = 0
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+totalAPagar = document.querySelector('.valor-total');
+totalAPagar.innerHTML = 0
